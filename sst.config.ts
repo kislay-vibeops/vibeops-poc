47b767dcf89ff5e79ee6a07551bf5e82c5dbb8a7
Kislay007 <kislaykishu@gmail.com>
2025-07-14 23:11:20 +0530

    new app name

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "test-repo-2",
+      name: "test-repo-3",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
